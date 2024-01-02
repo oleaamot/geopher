c65bb7e5d105316e37da546444ed0b7330cee69f
Ole Aamot <ole@aamot.org>
2024-01-02 02:27:55 +0100

    Geopher Movement-based Search Engine (20240102) 4.0

--- www/location/name/search/submit/index.php
+++ www/location/name/search/submit/index.php
@@ -12,7 +12,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     // TODO: Implement validation and error handling
 
      // Log the form data
-    $logMessage = "Movement Search Request - Initiator Email: $initiatorEmail, Target Email: $targetEmail, Price: $price, Time Period: $timePeriod\n";
+    $logMessage = "Movement Search Request - Initiator Email: $initiatorEmail, Target Email: $targetEmail, Price: $price, Time Period: $timePeriod, Latitude: $glat, Longitude: $glon, Altitude: $galt\n";
     error_log($logMessage, 3, '/home/5/g/geopher/log/logfile.log');
 
     // Prepare the email subject and message
@@ -21,11 +21,12 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $message .= "Target Email: $targetEmail\n";
     $message .= "Price: $price\n";
     $message .= "Time Period: $timePeriod\n";
-    $message .= "<a href='https://geopher.com/location/name/locate/?initiatorEmail=" . $initiatorEmail . "&glat=" . $glat . "&glon=" . $glon . "&id=abcdefghijklmnopqrs'>https://geopher.com/location/name/locate/?initiatorEmail=" . $initiatorEmail . "&glat=" . $glat . "&glon=" . $glon . "&id=abcdefghijklmnopqrs</a>";
-    $message .= "<a href='https://geopher.com/?query=&radius=50&search=Go&glat=" . $glat . "&glon=" . $glon . "&galt=" . $galt . "&initiatorEmail=" . $initiatorEmail . "#results'>https://geopher.com/?query=&radius=50&search=Go&glat=" . $glat . "&glon=" . $glon . "&galt=" . $galt . "&initiatorEmail=" . $initiatorEmail . "#results</a>";
+    $message .= "Respond to Initiator: <a href='https://geopher.com/location/name/locate/?initiatorEmail=" . $initiatorEmail . "&glat=" . $glat . "&glon=" . $glon . "&id=abcdefghijklmnopqrs'>https://geopher.com/location/name/locate/?initiatorEmail=" . $initiatorEmail . "&glat=" . $glat . "&glon=" . $glon . "&id=abcdefghijklmnopqrs</a>\n";
+    $message .= "Location of Initiator: <a href='https://geopher.com/?query=&radius=50&search=Go&glat=" . $glat . "&glon=" . $glon . "&galt=" . $galt . "&initiatorEmail=" . $initiatorEmail . "#results'>https://geopher.com/?query=&radius=50&search=Go&glat=" . $glat . "&glon=" . $glon . "&galt=" . $galt . "&initiatorEmail=" . $initiatorEmail . "#results</a>";
     // Send the email
     $recipient = $targetEmail; // Replace with the actual recipient email address
-    $headers = "From: Geopher Movement Search <noreply@geopher.com>";
+    $headers = "From: Geopher Movement Search <noreply@geopher.com>\n";
+    $headers .= "Reply-To: Geopher Movement Name <" . $initiatorEmail . ">\n";
 
     $success = mail($recipient, $subject, $message, $headers);
 
